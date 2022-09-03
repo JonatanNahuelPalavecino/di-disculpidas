b46a14b3afa8bc51d63be2f4c829282334c2ad7e
Jonatan Palavecino <jnpr1992@hotmail.com>
2022-09-03 18:49:25 -0300

    Primer commit: Pagina de DW finalizada con API para recibir formulario

---
+++ js/archivo.js
@@ -0,0 +1,14 @@
+const btnEnviar = document.getElementById('btn-enviar');
+const contenedor = document.getElementById('body')
+
+btnEnviar.addEventListener ('click', () => {
+    const div = document.createElement('div')
+    div.className = "mensaje"
+    div.innerHTML = `
+                    <h2>Mensaje Enviado</h2>
+    `
+    contenedor.appendChild(div)
+    setTimeout(() => {
+        div.remove()
+    }, 3000);
+})
